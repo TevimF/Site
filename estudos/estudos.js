86a4b5595fb09616de43c883c05219b4d5678e8c
Estêvão Felipe <115885688+TevimF@users.noreply.github.com>
2024-03-20 22:22:24 +0000

    algumas façanhas de js

---
+++ estudos/estudos.js
@@ -0,0 +1,25 @@
+
+    // declaração de variaveis
+    var nome = "Estevao";
+    // acessivel dentro da função que foi declarada. Ela tem escopo de função
+    let idade;
+    // acessivel dentro do bloco que foi declarada. Ela tem escopo de bloco
+    const data_n = "07/03/2002";
+    // também é bloqueada, mas não pode ser alterada
+
+    // a tipagem é dinamica, ou seja, a variavel pode mudar de tipo e não precisa ser declarada especificando o tipo
+
+    // a diferença de var e let é que var pode ser redeclarada e let não. isso significa que você pode declarar a mesma variavel mais de uma vez com var e não com let.
+    // exemplo:
+    nome = "Estevao Felipe";
+    // vai imprimir "Estevao Felipe" mesmo que a variavel já tenha sido declarada
+
+    /* let idade = 20;
+    let idade = 21;
+    console.log(idade); */
+    // daria erro, pois a variavel já foi declarada
+
+    // para imprimir no console é só usar console.log()
+    console.log(nome, idade, data_n);
+    // data_n ficou azul pois é uma constante
+    
